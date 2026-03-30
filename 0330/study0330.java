56deec07005c06dfe66e9233443909e418fad91c
geonwoo <gyn9208@gachon.ac.kr>
2026-03-30 21:14:18 +0900

    수정사항 수정했습니다.

--- 0330/study0330.java
+++ 0330/study0330.java
@@ -20,7 +20,7 @@ public class study0330 {
   public static int ScanNum(){
     while(true) {
       int num = 0;
-      System.out.print("저장할 아기사자 수를 5 이상 입력해주세요. ");
+      System.out.print("저장할 아기사자 수를 5 이상 입력해주세요");
       num = Integer.parseInt(scanner.nextLine()); //사람 몇 명인지 num에 입력받아 저장
       if (num < 5) {
         System.out.println("[오류] 5 이상 입력해주세요.");
