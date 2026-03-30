cbab03b0ee8eb0e03749e841102bcc6513ab42a6
geonwoo <gyn9208@gachon.ac.kr>
2026-03-30 21:04:04 +0900

    요청하신 메소드 수정했습니다

--- 0330/study0330.java
+++ 0330/study0330.java
@@ -1,20 +1,11 @@
 import java.util.Scanner;
 
 public class study0330 {
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    int num = 0;
+  static Scanner scanner = new Scanner(System.in);
 
-    while(true) {
-      System.out.print("저장할 아기사자 수를 5 이상 입력해주세요. ");
-      num = Integer.parseInt(scanner.nextLine()); //사람 몇 명인지 num에 입력받아 저장
-      if (num < 5) {
-        System.out.println("[오류] 5 이상 입력해주세요");
-      } else{
-        break;
-      }
-    }
-      String[] names = new String[num];
+  public static void main(String[] args) {
+
+      String[] names = new String[ScanNum()];
 
       for (int i = 0; i < names.length; i++) {
         System.out.print((i + 1) + "번째 아기사자: ");
@@ -24,5 +15,18 @@ public class study0330 {
       for(int i=0; i < names.length; i++){
         System.out.println("저는 아기사자 " + names[i] + "입니다 ㅎㅎ");
       }
+  }
+
+  public static int ScanNum(){
+    while(true) {
+      int num = 0;
+      System.out.print("저장할 아기사자 수를 5 이상 입력해주세요. ");
+      num = Integer.parseInt(scanner.nextLine()); //사람 몇 명인지 num에 입력받아 저장
+      if (num < 5) {
+        System.out.println("[오류] 5 이상 입력해주세요");
+      } else{
+        return num;
+      }
+    }
   }
 }
